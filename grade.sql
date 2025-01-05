b48cf17b05d411823e581609edf95163a45ed9d0
dch66 <15535517032@163.com>
2025-01-05 11:19:31 +0800

    数据库grade表添加成绩唯一特性

--- grade.sql
+++ grade.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50726 (5.7.26)
  File Encoding         : 65001
 
- Date: 04/01/2025 16:22:42
+ Date: 05/01/2025 11:18:54
 */
 
 SET NAMES utf8mb4;
@@ -34,16 +34,51 @@ CREATE TABLE `course`
 ) ENGINE = MyISAM CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
+-- Records of course
+-- ----------------------------
+INSERT INTO `course`
+VALUES (1101, 'java', '实验楼213', 6, '周一 1-2节', 9);
+INSERT INTO `course`
+VALUES (1101, 'java', '实验楼213', 6, '周一 1-2节', 8);
+INSERT INTO `course`
+VALUES (101, '数学', 'A101', 1, '2025-01-05', 2);
+INSERT INTO `course`
+VALUES (102, '英语', 'A102', 1, '2025-01-06', 3);
+INSERT INTO `course`
+VALUES (103, '物理', 'A103', 1, '2025-01-07', 5);
+INSERT INTO `course`
+VALUES (104, '化学', 'A104', 1, '2025-01-08', 2);
+
+-- ----------------------------
 -- Table structure for grade
 -- ----------------------------
 DROP TABLE IF EXISTS `grade`;
-CREATE TABLE `grade`  (
-  `studentId` int(10) NOT NULL,
-  `courseId` int(10) NULL DEFAULT NULL,
-  `grade` int(10) NULL DEFAULT NULL,
-  INDEX `student_id`(`studentId`) USING BTREE,
-  INDEX `course_id`(`courseId`) USING BTREE
+CREATE TABLE `grade`
+(
+    `studentId` int(10) NOT NULL,
+    `courseId`  int(10) NULL DEFAULT NULL,
+    `grade`     int(10) NULL DEFAULT NULL,
+    INDEX       `course_id`(`courseId`) USING BTREE,
+    UNIQUE INDEX `studentId`(`studentId`, `courseId`) USING BTREE
 ) ENGINE = MyISAM CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = Fixed;
+
+-- ----------------------------
+-- Records of grade
+-- ----------------------------
+INSERT INTO `grade`
+VALUES (8, 1101, 100);
+INSERT INTO `grade`
+VALUES (9, 1101, 88);
+INSERT INTO `grade`
+VALUES (2, 101, 90);
+INSERT INTO `grade`
+VALUES (3, 102, 85);
+INSERT INTO `grade`
+VALUES (5, 103, 95);
+INSERT INTO `grade`
+VALUES (2, 104, 88);
+INSERT INTO `grade`
+VALUES (8, 101, 99);
 
 -- ----------------------------
 -- Table structure for users
@@ -59,7 +94,29 @@ CREATE TABLE `users`
     `phone`    varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL,
     PRIMARY KEY (`id`) USING BTREE,
     INDEX      `name`(`name`) USING BTREE
-) ENGINE = MyISAM AUTO_INCREMENT = 6 CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = Dynamic;
+) ENGINE = MyISAM AUTO_INCREMENT = 15 CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of users
+-- ----------------------------
+INSERT INTO `users`
+VALUES (4, 'admin', 'admin', 'qwerty', 'admin', '11111111111');
+INSERT INTO `users`
+VALUES (6, '10001', '赵亮', '123456', 'teacher', '17188495598');
+INSERT INTO `users`
+VALUES (7, '10002', '马千里', '123456', 'teacher', '19188559999');
+INSERT INTO `users`
+VALUES (8, '2220020305', 'hyy', '123456', 'student', '13133665599');
+INSERT INTO `users`
+VALUES (9, '2220020308', 'aad', '123456', 'student', '78945641231');
+INSERT INTO `users`
+VALUES (1, 'teacher1', '张老师', 'password123', 'teacher', '12345678901');
+INSERT INTO `users`
+VALUES (2, 'student1', '李学生', 'password123', 'student', '12345678902');
+INSERT INTO `users`
+VALUES (3, 'student2', '王学生', 'password123', 'student', '12345678903');
+INSERT INTO `users`
+VALUES (5, 'student3', '赵学生', 'password123', 'student', '12345678905');
 
 SET
 FOREIGN_KEY_CHECKS = 1;
