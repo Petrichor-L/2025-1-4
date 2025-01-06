62748faeed7067768ef6aea0a79dea70633384a5
LT <15338166+petrichor_l@user.noreply.gitee.com>
2025-01-06 09:33:13 +0800

    no message

--- VUE/src/views/student/Courses.vue
+++ VUE/src/views/student/Courses.vue
@@ -9,6 +9,11 @@
       <el-table-column prop="courseId" label="课程编号" width="180" />
       <el-table-column prop="courseName" label="课程名称" width="180" />
       <el-table-column prop="teacherName" label="任课教师" width="120" />
+      <el-table-column label="上课时间" width="180" align="center">
+        <template #default="scope">
+          {{ generateClassTime() }}
+        </template>
+      </el-table-column>
       <el-table-column prop="classroom" label="上课教室" />
     </el-table>
   </div>
@@ -21,6 +26,23 @@ import { getStudentCourses } from '@/api/student'
 
 const courseList = ref([])
 const loading = ref(false)
+
+// 生成随机上课时间
+const generateClassTime = () => {
+  const weekdays = ['一', '二', '三', '四', '五']
+  const sections = [
+    '1-2节',
+    '3-4节',
+    '5-6节',
+    '7-8节',
+    '9-10节'
+  ]
+  
+  const randomWeekday = weekdays[Math.floor(Math.random() * weekdays.length)]
+  const randomSection = sections[Math.floor(Math.random() * sections.length)]
+  
+  return `星期${randomWeekday} 第${randomSection}`
+}
 
 // 加载课程列表
 const loadCourses = async () => {
