65bb13aee18192930a08e1d30b7967bfec7d13d8
dch66 <15535517032@163.com>
2025-01-06 14:32:02 +0800

    新的数据库

--- _localhost-2025_01_05_16_10_48-dump.sql
+++ _localhost-2025_01_05_16_10_48-dump.sql
@@ -1,61 +1,94 @@
--- MySQL dump 10.13  Distrib 8.0.40, for Win64 (x86_64)
---
--- Host: 127.0.0.1    Database: student_performance_management_system
--- ------------------------------------------------------
--- Server version	8.0.40
+/*
+ Navicat Premium Data Transfer
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+ Source Server         : localhost_3306
+ Source Server Type    : MySQL
+ Source Server Version : 50726 (5.7.26)
+ Source Host           : localhost:3306
+ Source Schema         : student_performance_management_system
 
---
--- Dumping data for table `course`
---
+ Target Server Type    : MySQL
+ Target Server Version : 50726 (5.7.26)
+ File Encoding         : 65001
 
-LOCK TABLES `course` WRITE;
-/*!40000 ALTER TABLE `course` DISABLE KEYS */;
-INSERT INTO `course` VALUES (1102,'利于',NULL,NULL),(12123,'jabj',NULL,NULL),(12312,'ashfiq',NULL,NULL),(3231312,'jsdai',NULL,NULL),(2318731,'java',NULL,10001);
-/*!40000 ALTER TABLE `course` ENABLE KEYS */;
-UNLOCK TABLES;
+ Date: 06/01/2025 14:31:09
+*/
 
---
--- Dumping data for table `grade`
---
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
 
-LOCK TABLES `grade` WRITE;
-/*!40000 ALTER TABLE `grade` DISABLE KEYS */;
-INSERT INTO `grade` VALUES ('10001',1101,99),('222923121',1101,95),('222923121',1102,95),('222923121',12123,95);
-/*!40000 ALTER TABLE `grade` ENABLE KEYS */;
-UNLOCK TABLES;
+-- ----------------------------
+-- Table structure for course
+-- ----------------------------
+DROP TABLE IF EXISTS `course`;
+CREATE TABLE `course`  (
+  `course_id` int(10) UNSIGNED NOT NULL,
+  `course_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL,
+  `classroom` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL,
+  `teacher_id` int(10) NULL DEFAULT NULL,
+  PRIMARY KEY (`course_id`) USING BTREE,
+  INDEX `teacher_id`(`teacher_id`) USING BTREE
+) ENGINE = MyISAM CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = DYNAMIC;
 
---
--- Dumping data for table `users`
---
+-- ----------------------------
+-- Records of course
+-- ----------------------------
+INSERT INTO `course` VALUES (1, 'c1', 'c1', 2);
+INSERT INTO `course` VALUES (2, 'c2', 'c2', 2);
+INSERT INTO `course` VALUES (3, 'c3', 'c3', 3);
+INSERT INTO `course` VALUES (4, 'c4', 'c4', 3);
 
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'test001','123456','student','12312312',NULL),(2,'10001','123456','teacher','13800138000','王大山'),(3,'2220020310','123456','admin','',NULL),(5,'2220020102','123456','student','13356237732',NULL),(6,'222923121','123456','student','13332322121',NULL),(7,'2220020312','123456','student','13267323912',NULL),(9,'2220030230','123456','student','15632894356','孙武'),(10,'22122234565','123456','student','13358849207','李桐'),(12,'2220020333','123456','student','13346798520','王鑫'),(13,'2221023102','122345','student','13369748512','孙萨芬hi'),(14,'2220020111','123456','teacher','12345686214','sazdh');
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
+-- ----------------------------
+-- Table structure for grade
+-- ----------------------------
+DROP TABLE IF EXISTS `grade`;
+CREATE TABLE `grade`  (
+  `studentId` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `courseId` int(11) NULL DEFAULT NULL,
+  `grade` int(11) NULL DEFAULT NULL,
+  UNIQUE INDEX `studentId`(`studentId`, `courseId`) USING BTREE,
+  INDEX `course_id`(`courseId`) USING BTREE
+) ENGINE = MyISAM CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = FIXED;
 
---
--- Dumping routines for database 'student_performance_management_system'
---
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+-- ----------------------------
+-- Records of grade
+-- ----------------------------
+INSERT INTO `grade` VALUES ('7', 1, NULL);
+INSERT INTO `grade` VALUES ('6', 2, 22);
+INSERT INTO `grade` VALUES ('5', 1, 121);
+INSERT INTO `grade` VALUES ('5', 2, 35);
+INSERT INTO `grade` VALUES ('4', 2, 77);
+INSERT INTO `grade` VALUES ('4', 1, 60);
+INSERT INTO `grade` VALUES ('7', 2, NULL);
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+-- ----------------------------
+-- Table structure for users
+-- ----------------------------
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users`  (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `role` enum('admin','teacher','student') CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `phone` varchar(20) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL,
+  `name` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`user_id`) USING BTREE,
+  UNIQUE INDEX `username`(`username`) USING BTREE
+) ENGINE = MyISAM AUTO_INCREMENT = 16 CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = Dynamic;
 
--- Dump completed on 2025-01-05 16:10:49
+-- ----------------------------
+-- Records of users
+-- ----------------------------
+INSERT INTO `users` VALUES (4, 's1', '123456', 'student', '12312312', '学生');
+INSERT INTO `users` VALUES (2, 't1', '123456', 'teacher', '13800138000', '王大山');
+INSERT INTO `users` VALUES (1, 'admin', '123456', 'admin', '12345678910', 'admin');
+INSERT INTO `users` VALUES (5, 's2', '123456', 'student', '13356237732', '李四');
+INSERT INTO `users` VALUES (6, 's3', '123456', 'student', '13332322121', '王五');
+INSERT INTO `users` VALUES (7, 's4', '123456', 'student', '13267323912', '王六');
+INSERT INTO `users` VALUES (8, 's5', '123456', 'student', '15632894356', '孙武');
+INSERT INTO `users` VALUES (9, 's6', '123456', 'student', '13358849207', '李桐');
+INSERT INTO `users` VALUES (10, 's7', '123456', 'student', '13346798520', '王鑫');
+INSERT INTO `users` VALUES (11, 's8', '123456', 'student', '13369748512', '孙萨芬hi');
+INSERT INTO `users` VALUES (3, 't2', '123456', 'teacher', '12345686214', '大苞米');
+
+SET FOREIGN_KEY_CHECKS = 1;
